f342aa28b14d3b0bc213cc404c5e8636547c88b7
h0ly byte <admin@kbve.com>
2016-08-07 00:30:22 +0000

    Updates

--- plugin/routes.js
+++ plugin/routes.js
@@ -14,6 +14,20 @@
             middleware    = payload.coreMiddleware,
             errorHandler  = payload.errorHandler;
 
+
+        router.get('/dashboard/:uid', function (req, res) {
+            user.getUserData(req.params.uid, function (error, userData) {
+                if(error){
+                    return errorHandler.respond(500, res);
+                }else if (!userData) {
+                    return errorHandler.respond(404, res);
+                }
+
+                res.json(userData);
+            });
+        });
+
+
         router.get('/users/:uid', apiMiddleware.requireUser, function (req, res) {
             user.getUserData(req.params.uid, function (error, userData) {
                 if(error){
